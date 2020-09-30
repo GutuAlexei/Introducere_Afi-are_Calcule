26494416fbc7d5d911287ba9d16f93be9f353c00
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-09-30 20:48:38 +0300

    Add files via upload

---
+++ problema_9_AG.py
@@ -0,0 +1,7 @@
+#Problema 9
+a=int(input('prima cifra='))
+b=int(input('a doua cifra='))
+c=int(input('a treia cifra='))
+print( a , '+' ,b , '=' , a+b )
+print( a , '+' ,c , '=' , a+c )
+print( b , '+' ,c , '=' , b+c )
