d5ccdca8ae04e154cd072778967a1b79a9c5b6e3
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-09-30 15:51:12 +0300

    Add files via upload

---
+++ problema_12_AG.py
@@ -0,0 +1,6 @@
+# Problema 12
+a=int(input('introduceti nr. de oua pe care le face 1 gaina intr-o zi='))
+b=int(input('introduceti nr. de oua pe care le face a 2 gaina intr-o zi='))
+c=int(input('introduceti nr. de oua pe care le face a 3 gaina intr-o zi='))
+d=int(input('introduceti nr. de oua pe care le face a 4 gaina intr-o zi='))
+print('intr-o saptamana se obtin' , 7*(a+b+c+d) )
