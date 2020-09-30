77f11b27dccc8c418c13485e57f9b8b904f208b4
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-09-30 16:04:03 +0300

    Add files via upload

---
+++ problema_11_AG.py
@@ -0,0 +1,5 @@
+# Problema 11
+a=int(input('numarul de iepuri de la ineput de luna='))
+b=int(input('nr. iepuri morti='))
+c=int(input('nr. iepuri nascuti='))
+print('iepuri,in data de iesire sunt' , a+c-b)
