c5b4380ee05e70ac6ff7f80cfb9a9410049a01b2
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-10-04 01:39:09 +0300

    Add files via upload

---
+++ Problema_1_AG.py
@@ -0,0 +1,17 @@
+a=int(input('intr. numarul='))
+if a==1:
+ print ('Rosu')
+if a==2:
+ print ('Oranj')
+if a==3:
+ print ('Galben')
+if a==4:
+ print ('Verde')
+if a==5:
+ print ('Albastru')
+if a==6:
+ print ('Indigo')
+if a==7:
+ print ('Violet')
+elif a>7:
+    print('dute si invata culorile ')
